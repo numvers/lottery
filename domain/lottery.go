8a6a1a062f2665f172d35f1ace83053890871a4b
raeperd <raeperd117@gmail.com>
2023-09-17 02:07:19 +0900

    First commit

---
+++ domain/lottery.go
@@ -0,0 +1,21 @@
+package domain
+
+type Lottery struct {
+	Round   uint
+	Numbers LotteryNumbers
+	Wins    []Win
+}
+
+type LotteryNumbers struct {
+	Numbers []uint
+}
+
+type Win struct {
+	NumWinners uint
+	Prize      uint
+}
+
+type LotteryRepoitoy interface {
+	FindAll() ([]Lottery, error)
+	FindByRound(round uint) (Lottery, error)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/numvers/lottery
+
+go 1.21
